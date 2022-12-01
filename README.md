6fde65dbfa15ad0f4f15bbfaf3c9bfc0a65e354f
Pierrick Foulquier <pierrickfoulquier@gmail.com>
2022-12-01 16:07:35 +0100

    Initial commit with working code

---
+++ README.md
@@ -0,0 +1,55 @@
+# Advent of Code 2022 - Day 1
+
+## Task 1
+Parse the input file and calculate the sum of each parapgraph.\
+Find the largest sum.
+
+## Task 2
+Find the three largest sums.
+Calculate their total.
+
+
+## Example
+
+```
+1
+
+1
+1
+
+1
+1
+1
+
+1
+1
+1
+1
+```
+
+The largest sum is:
+```
+1 + 1 + 1 + 1 = 4
+```
+The three largest sums are:
+```
+1 + 1 = 2
+```
+```
+1 + 1 + 1 = 3
+```
+```
+1 + 1 + 1 + 1 = 4
+```
+Their total is:
+```
+2 + 3 + 4 = 9
+```
+
+
+
+
+
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, Read};
+
+fn main() -> Result<(), Error> {
+    let elves = parse_file(File::open("input")?);
+    compare_values(elves);
+    Ok(())
+}
+
+fn parse_file<R: Read>(input: R) -> Vec<Vec<u32>> {
+    let br = BufReader::new(input);
+    let mut elves: Vec<Vec<u32>> = Vec::new();
+    let mut elf: Vec<u32> = Vec::new();
+    for line in br.lines() {
+        if line.as_ref().unwrap() != "" {
+            let number: u32 = line.expect("Error reading line").trim().parse().unwrap();
+            elf.push(number);
+        } else {
+            elves.push(elf.clone());
+            elf.clear();
+            continue
+        }
+    }
+    elves
+}
+
+fn compare_values(vec: Vec<Vec<u32>>) {
+    let mut totals: Vec<u32> = Vec::new();
+    for elf in vec {
+        totals.push(calculate_sum(elf));
+    }
+    find_three_largest(totals);
+}
+
+fn calculate_sum(vec: Vec<u32>) -> u32 {
+    vec.iter().sum()
+}
+
+fn _find_largest(vec: Vec<u32>) -> u32 {
+    *vec.iter().max().unwrap()
+}
+
+fn _print_largest(num: u32) {
+    println!("Largest value is {}", num.to_string());
+}
+
+fn find_three_largest(vec: Vec<u32>) {
+    let mut sorted = vec.clone();
+    sorted.sort();
+    sorted.reverse();
+    let total = sorted[0] + sorted[1] + sorted[2];
+    println!("{total}");
+}
